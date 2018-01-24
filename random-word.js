0ca3a4c998eb22ec4c7802579c5c1a613f90b189
Joseph-Marlitt <joseph.marlitt@gmail.com>
2018-01-24 17:49:19 -0600

    final

--- random-word.js
+++ random-word.js
@@ -1,22 +1,7 @@
+exports.newWord = {
+    arrayOfWords: ["ROCKY ROAD", "VANILLA", "MINT CHOCOLATE CHIP", "RAINBOW SHERBERT", "CARAMEL", "PISTACHIO"]
+};
 
-var arrayOfWords = ["rocky road", "vanilla", "mint chocolate chip", "rainbow sherbert", "caramel", "pistachio"];
-var randomWord = arrayOfWords[Math.floor(arrayOfWords.length * Math.random())];
-var splitWord = randomWord.split ("");
-var guesses = [];
-
-
-function LetterFunction(letter){
-    this.letter = letter,
-    this.guesses = false
-}
-
-for (var i = 0; i < randomWord.length; i++){
-    var letter = new LetterFunction(randomWord[i])
-    guesses.push(letter); 
-
-}
-
-module.exports = guesses;
 
 
 
